f92ae756345b3a9a9470976937c57ec66bc47792
NickFer5 <nicholas.ferington@me.com>
2025-07-05 21:25:15 -0400

    Delete forms.py

--- forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CarListing
-
-class CarListingForm(forms.ModelForm):
-    class Meta:
-        model  = CarListing
-        fields = ['year', 'make', 'model', 'trim', 'price', 'description']
