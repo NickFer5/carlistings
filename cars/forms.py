7980f5cf3cdad7138acaef226e7dc602df07ebe8
NickFer5 <nicholas.ferington@me.com>
2025-07-05 21:25:41 -0400

    Add files via upload

--- cars/forms.py
+++ cars/forms.py
@@ -4,4 +4,4 @@ from .models import CarListing
 class CarListingForm(forms.ModelForm):
     class Meta:
         model  = CarListing
-        fields = ['make', 'model', 'year', 'price', 'description']
+        fields = ['year', 'make', 'model', 'trim', 'price', 'description']
